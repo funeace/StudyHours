8cb3a9537ac72d9193b3c91cccaab8fb8248e93b
funeace <funeace@gmail.com>
2020-04-06 03:27:44 +0000

    [Add]Userモデルにプロフィール画像を投稿できるようカラムを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_025257) do
+ActiveRecord::Schema.define(version: 2020_04_06_032422) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "room_id", null: false
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_025257) do
     t.string "meta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
