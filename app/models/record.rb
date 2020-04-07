c843baee6b6f14d48b8765a6373d4926cb4e426e
funeace <funeace@gmail.com>
2020-04-07 01:27:15 +0000

    [Add]学習記録にいいね機能を追加しました

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,4 +3,9 @@ class Record < ApplicationRecord
   has_many :record_details, dependent: :destroy
   has_many :record_favorites ,dependent: :destroy
   has_many :record_comments ,dependent: :destroy
+
+  # 既にいいね ボタンを押しているか確認
+  def favorited_by?(user)
+    self.record_favorites.where(user_id: user.id).exists?
+  end
 end
