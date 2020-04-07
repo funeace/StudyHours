be38fad9d95103af0d8840731fb594d42c46dfb6
funeace <funeace@gmail.com>
2020-04-07 15:41:25 +0000

    [Add]ログイン後にタイムラインが表示されるように設定しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   # deviseの設定
-  # Ajaxリクエストにセキュリティトークンを含ませる
+  # Ajaxリクエストにセキュリティトークンを含ませる(OmniAuth)
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    end
+
+    def after_sign_in_path_for(resource)
+      timelines_path
     end
 end
