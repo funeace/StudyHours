bede09136d39861ffd82fc927915d9fae25b7be1
funeace <funeace@gmail.com>
2020-04-08 10:59:08 +0000

    [Add]学習記録のnew/createを実装しました

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -1,2 +1,18 @@
 class StudyLogsController < ApplicationController
+  def new
+    @study_log = current_user.study_logs.new
+    @study_log.study_log_details.build
+  end
+
+  def create
+    @study_log = current_user.study_logs.new(study_log_params)
+    if @study_log.save
+      flash[:notice] = "登録が完了しました。この調子で頑張りましょう"
+      redirect_to timelines_path
+    end
+  end
+private
+  def study_log_params
+    params.require(:study_log).permit(:working_date,:memo,study_log_details_attributes:[:tag_list,:hour,:min])
+  end
 end
