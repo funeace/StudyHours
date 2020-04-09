a8f941daf8e1cf858fd1c47462ae8e5eebdce2d8
funeace <funeace@gmail.com>
2020-04-09 01:18:17 +0000

    [Add]学習記録のdestroyを実装しました

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -25,16 +25,28 @@ class StudyLogsController < ApplicationController
 
   def update
     @study_log = StudyLog.find(params[:id])
-    @study_log.update(study_log_update_params)
-    flash[:notice] = "更新が完了しました。"
-    redirect_to study_log_path(@study_log)
+    if @study_log.update(study_log_update_params)
+      flash[:notice] = "更新が完了しました。"
+      redirect_to study_log_path(@study_log)
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    study_log = StudyLog.find(params[:id])
+    study_log.destroy
+    flash[:notice] = "投稿を削除しました。"
+    redirect_to timelines_path
+  end
+
 
 private
   def study_log_params
     params.require(:study_log).permit(:working_date,:memo,study_log_details_attributes:[:tag_list,:hour,:min])
   end
 
+  # update内で自分のレコードを判定させるためにidを持たせる
   def study_log_update_params
     params.require(:study_log).permit(:working_date,:memo,study_log_details_attributes:[:tag_list,:hour,:min,:id,:_destroy])
   end
