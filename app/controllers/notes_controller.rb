1ced968f53980bd7883a0ab9e1218e0de15e1bc5
funeace <funeace@gmail.com>
2020-04-09 08:21:21 +0000

    [Add]ノートのnew/createを実装しました

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,19 @@
 class NotesController < ApplicationController
+  def new
+    @note = current_user.notes.new
+  end
+  def create
+    @note = current_user.notes.new(note_params)
+    if @note.save
+      flash[:notice] = "ノートを投稿しました。"
+    else
+      render 'new'
+    end
+    redirect_to timelines_path
+  end
+
+private
+  def note_params
+    params.require(:note).permit(:user_id,:title,:body,:tag_list)
+  end
 end
