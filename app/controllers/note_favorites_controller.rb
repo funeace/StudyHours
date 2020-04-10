f295cb28b995846f1c25ec5ba672094ae208fda6
funeace <funeace@gmail.com>
2020-04-10 13:17:51 +0000

    [Add]学習記録のいいね機能（非同期通信）

--- app/controllers/note_favorites_controller.rb
+++ app/controllers/note_favorites_controller.rb
@@ -4,13 +4,18 @@ class NoteFavoritesController < ApplicationController
   def create
     @note = Note.find(params[:note_id])
     @note.note_favorites.create(user_id: current_user.id)
-    redirect_back(fallback_location: root_path)   
+    # redirect_back(fallback_location: root_path)   
   end
 
   def destroy
     @note = Note.find(params[:note_id])
     # binding.pry
     @note.note_favorites.find_by(user_id: current_user.id).destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+  end
+
+private
+  def set_note_favorite
+    @note = Note.find(params[:note_id])
   end
 end
--- app/controllers/study_log_favorites_controller.rb
+++ app/controllers/study_log_favorites_controller.rb
@@ -1,16 +1,21 @@
 class StudyLogFavoritesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_study_log
 
     def create
     @study_log = StudyLog.find(params[:study_log_id])
     @study_log.study_log_favorites.create(user_id: current_user.id)
-    redirect_back(fallback_location: root_path)   
+    # redirect_back(fallback_location: root_path)   
   end
 
   def destroy
     @study_log = StudyLog.find(params[:study_log_id])
     # binding.pry
     @study_log.study_log_favorites.find_by(user_id: current_user.id).destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+  end
+private
+  def set_study_log
+    @study_log = StudyLog.find(params[:study_log_id])
   end
 end
