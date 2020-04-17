da01446232ceaedf8f6ca617109b3962c6d000ba
funeace <funeace@gmail.com>
2020-04-17 00:35:41 +0000

    [Fix]チャットの投稿者を判定できるよう修正しました

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -2,13 +2,19 @@ class ChatBroadcastJob < ApplicationJob
   queue_as :default
 
   # データ保存後に画面を更新する処理
-  def perform(chat)
-    ActionCable.server.broadcast("room_channel_#{chat.room_id}",chat: render_chat(chat))
+  def perform(chat, current_user)
+    ActionCable.server.broadcast("room_channel_#{chat.room_id}",chat: render_chat(chat,current_user))
   end
 
 private
   # コントローラからrenderを行うための処理
-  def render_chat(chat)
-    ApplicationController.renderer.render(partial: 'chats/chat',locals: { chat: chat })
+  def render_chat(chat, current_user)
+    if current_user == chat.user_id
+      # ログインユーザーなので右側向けのcssを追加する
+      ApplicationController.renderer.render(partial: 'chats/login_user',locals: { chat: chat,user: current_user })
+    else
+      # 他のユーザーなので左側に表示する
+      ApplicationController.renderer.render(partial: 'chats/other_user',locals: { chat: chat,user: current_user })
+    end
   end
 end
