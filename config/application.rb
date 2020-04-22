b7ec84277fa61664442565d2fafd0253df6a750a
funeace <funeace@gmail.com>
2020-04-22 07:21:01 +0000

    [Add]Deviseの日本語化を行いました

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module StudyHours
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # タイムゾーンの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # 日本語を優先させる
+    config.i18n.default_locale = :ja
+    # パスを設定
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
