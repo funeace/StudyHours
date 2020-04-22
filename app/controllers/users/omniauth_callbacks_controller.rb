f522d0ff9991351dfe0c5905059f845323433a39
funeace <funeace@gmail.com>
2020-04-22 06:24:55 +0000

    [Fix]論理削除されたomniauthのユーザでログインするとエラーになる問題を修正しました

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,17 +4,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # twitterのOmniAuth
   def twitter
-    flash[:notice] = "ログインに成功しました"
-    callback(:twitter)
+    if invalid_user?(:twitter)
+    # 既に退会済みユーザか判定
+      redirect_to root_path
+      flash[:danger] = "無効なユーザです"
+    else
+      callback(:twitter)
+      flash[:success] = "ログインに成功しました"
+    end
   end
 
   # googleのOmniAuth
   def google
     # binding.pry
-    flash[:notice] = "ログインに成功しました"
-    callback(:google)
+    if invalid_user?(:google)
+      redirect_to root_path
+      flash[:danger] = "無効なユーザです"
+    else
+      callback(:google)
+      flash[:success] = "ログインに成功しました"
+    end
   end
-
 
 private
   # googleもtwitterも、プロバイダー以外は同様の処理を行うためメソッドにまとめる
@@ -27,6 +37,17 @@ private
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end    
+  end
+
+  # 既に退会済みのユーザか判定するためのメソッド
+  def invalid_user?(provider)
+    if provider == :twitter
+      invalid = User.with_deleted.find_by(email: User.dummy_email(request.env["omniauth.auth"])).nil?
+    elsif provider == :google
+      invalid = User.with_deleted.find_by(email: request.env["omniauth.auth"]["info"]["email"]).nil?
+    end
+    # invalidを反転させることでvalid = trueならば正を作る
+    return !invalid
   end
 
   # More info at:
