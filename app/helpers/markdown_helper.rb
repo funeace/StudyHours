9aa5f56209abce1e3aaf1791d2267cb890fb42c9
funeace <funeace@gmail.com>
2020-04-26 17:33:42 +0000

    [Add]Noteのマークダウンにシンタックスハイライトを実装しました

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,7 +1,34 @@
 module MarkdownHelper
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    # 
+    def block_code(code, language)
+      language = language.split(':')[0]
+      case language.to_s
+        when 'rb'
+          lang = 'ruby'
+        when 'js'
+          lang = 'JavaScript'
+        when 'sql'
+          lang = 'SQL'
+        when 'yml'
+          lang = 'yaml'
+        when 'css'
+          lang = 'css'
+        when 'html'
+          lang = 'html'
+        when ''
+          lang = 'md'
+        else
+          lang = language
+      end
+      CodeRay.scan(code, lang).div
+    end
+  end
+
   def markdown(text)
-    unless @markdown
-      options = {
+    # 画面の入力内容からシンタックスハイライトを作成(with_toc_data: リンクを許可する設定)
+    html_render = HTMLwithCoderay.new(with_toc_data: true)
+    options = {
         # htmlのアクションを無効にうる
         filter_html: true,
         # 行末に２つスペースを入れなくても改行できるようになる
@@ -15,12 +42,15 @@ module MarkdownHelper
         # ---　または '''とするとコードと識別されるようになる
         fenced_code_blocks: true,
         # 引用符を解析
-        quote: true
-      }
-      renderer = Redcarpet::Render::HTML.new(options)
-      @markdown = Redcarpet::Markdown.new(renderer)
-    end
-
+        quote: true,
+        # ~２つで取り消し線
+        strikethrough: true,
+        # 下線
+        underline: true
+    }
+    # HTMLwithCoderayの中で既にRedcarpet::Render::HTMLを既に行っているため無効化
+    # renderer = Redcarpet::Render::HTML.new(option)
+    @markdown = Redcarpet::Markdown.new(html_render,options)
     @markdown.render(text).html_safe
   end
 end
