1d1e1a9f21594fc2613a7853809d107b7a1f45a2
funeace <funeace@gmail.com>
2020-04-27 06:50:00 +0000

    [Fix]Searchコントローラ

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -4,26 +4,122 @@ class SearchsController < ApplicationController
   def index
     @search_id = params[:search_id]
     @tag_name = params[:tag_name]
-    # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
-    @study_log_length = StudyLog.all.size
-    @note_length = Note.all.size
-    @user_length = User.all.size
+
+    # 共通して検索する項目をベースとして定義
+    users_base = User.all.includes([:relationships]).order(id: "DESC")
+    notes_base = Note.all.includes([:user,:note_comments,:note_favorites,:tags]).order(id: "DESC")
+    study_logs_base = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC")
+
+    # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得    
+    @study_log_length = study_logs_base.size
+    @note_length = notes_base.size
+    @user_length = users_base.size
     # 画面から受け取ったsearch_idがnilの場合は全件表示
     case @search_id
     when nil
-      @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
-      @notes = Note.all.includes([:user,:note_comments,:note_favorites,:tags]).order(id: "DESC").limit(LIMIT)
-      @study_logs = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").limit(LIMIT)
+      @users = users_base.limit(LIMIT)
+      @notes = notes_base.limit(LIMIT)
+      @study_logs = study_logs_base.limit(LIMIT)
 
     # tagで検索情報を受け取った時はtagに紐づいた情報を表示
     when "1"
-      @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
-      @notes = Note.tagged_with(@tag_name).limit(LIMIT)
-      @study_logs = StudyLog.tagged_with(@tag_name).limit(LIMIT)
+      @users = users_base.limit(LIMIT)
+      @notes = notes_base.tagged_with(@tag_name).limit(LIMIT)
+      @study_logs = study_logs_base.tagged_with(@tag_name).limit(LIMIT)
     end
   end
 
-  # もっと見るを押した時の処理
+  # ユーザの並び替えが押された時に実行する処理
+  def sort
+    # binding.pry
+    @user = current_user
+    @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
+    @notes = Note.all.includes([:user,:note_comments,:note_favorites,:tags]).order(id: "DESC").limit(LIMIT)
+    @study_logs = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").limit(LIMIT)
+    # binding.pry
+
+    sort = params[:sort_id]
+    case sort
+    # お気に入り順に並び替える
+    when "study_favorite_sort" then
+      @study_logs = StudyLog.all.left_joins(:study_log_favorites).group(:id).select('study_logs.*,COUNT("study_log_favorites"."id") AS favorites_count').order(favorites_count: "DESC").limit(LIMIT)
+    # 作成日順
+    when "study_created_sort" then
+      @study_logs
+    # ノートお気に入り順
+    when "note_favorite_sort" then
+      @notes = Note.all.left_joins(:note_favorites).group(:id).select('notes.*,COUNT("note_favorites"."id") AS note_count').order(note_count: "DESC").limit(LIMIT)
+    # ノート作成日順
+    when "note_created_sort" then
+      @notes
+    # フォロワー数
+    when "user_follower_sort" then
+      @users = User.all.left_joins(:reverse_of_relationships).group(:id).select('users.*,COUNT("relationships"."id") AS follower_count').order(follower_count: "DESC").limit(LIMIT)
+      # ユーザ作成日順
+    when "user_created_sort" then
+      @users
+    end
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  # ユーザ検索
+  def search
+    genre = params[:genre]
+    # binding.pry
+    @user = current_user
+    # 名前検索が行われた場合
+    if genre == "name"
+      # 共通して検索する項目をベースとして定義
+      users_base = User.where('name LIKE(?)',"%#{params[:keyword]}%")
+      notes_base = Note.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%"))
+      study_log_base = StudyLog.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%"))
+
+      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
+      @user_length = users_base.size
+      @note_length = notes_base.size
+      @study_log_length = study_log_base.size
+      # 検索する処理
+      @users = users_base.limit(LIMIT)
+      @notes = notes_base.limit(LIMIT)
+      @study_logs = study_log_base.limit(LIMIT)
+    # ノート検索
+    elsif genre == "note"
+      # 共通して検索する項目をベースとして定義
+      users_base = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC")
+      study_logs_base = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC")
+      notes_base = Note.where('title LIKE(?)',"%#{params[:keyword]}%").order(id: "DESC")
+      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
+      @user_length = users_base.size
+      @study_log_length = study_logs_base.size
+      @note_length = note_base.size
+      # 検索する処理
+      @users = users_base.limit(LIMIT)
+      @study_logs = study_logs_base.limit(LIMIT)
+      @notes = notee_base.limit(LIMIT)
+    # タグ検索(名前は引かない)
+    elsif genre =="tag"
+      # 共通して検索する項目をベースとして定義
+      users_base = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC")
+      study_logs_base = StudyLog.tagged_with(params[:keyword])
+      notes_base = Note.tagged_with(params[:keyword])
+      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
+      @user_log_length = users_base.size
+      @study_log_length = study_logs_base.size
+      @note_length = notes_base.size
+      # 検索する処理
+      @users = users_base.limit(LIMIT)
+      @study_logs =  study_logs_base.limit(LIMIT)
+      @notes = notes_base.limit(LIMIT)
+    end
+    # タグ検索
+    respond_to do |format|
+      format.json
+    end
+  end
+
+    # もっと見るを押した時の処理
   def more
     # 各々のデータ
     @user = current_user
@@ -70,7 +166,7 @@ class SearchsController < ApplicationController
     else
       # キーワード検索の場合
       if genre == "name"
-        @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").limit(LIMIT).offset(offset)
+        @users = User.all.includes([:relationships]).where('name LIKE(?)',"%#{params[:keyword]}%").limit(LIMIT).offset(offset)
         @notes = Note.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).limit(LIMIT).offset(offset)
         @study_logs = StudyLog.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).limit(LIMIT).offset(offset)
         # ノート検索
@@ -88,81 +184,4 @@ class SearchsController < ApplicationController
     end
   end
 
-  # ユーザの並び替えが押された時に実行する処理
-  def sort
-    # binding.pry
-    @user = current_user
-    @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
-    @notes = Note.all.includes([:user,:note_comments,:note_favorites,:tags]).order(id: "DESC").limit(LIMIT)
-    @study_logs = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").limit(LIMIT)
-    # binding.pry
-
-    sort = params[:sort_id]
-    case sort
-    # お気に入り順に並び替える
-    when "study_favorite_sort" then
-      @study_logs = StudyLog.all.left_joins(:study_log_favorites).group(:id).select('study_logs.*,COUNT("study_log_favorites"."id") AS favorites_count').order(favorites_count: "DESC").limit(LIMIT)
-    # 作成日順
-    when "study_created_sort" then
-      @study_logs
-    # ノートお気に入り順
-    when "note_favorite_sort" then
-      @notes = Note.all.left_joins(:note_favorites).group(:id).select('notes.*,COUNT("note_favorites"."id") AS note_count').order(note_count: "DESC").limit(LIMIT)
-    # ノート作成日順
-    when "note_created_sort" then
-      @notes
-    # フォロワー数
-    when "user_follower_sort" then
-      @users = User.all.left_joins(:reverse_of_relationships).group(:id).select('users.*,COUNT("relationships"."id") AS follower_count').order(follower_count: "DESC").limit(LIMIT)
-      # ユーザ作成日順
-    when "user_created_sort" then
-      @users
-    end
-    respond_to do |format|
-      format.json
-    end
-  end
-
-  # ユーザ検索
-  def search
-    genre = params[:genre]
-    # binding.pry
-    @user = current_user
-    # 名前検索が行われた場合
-    if genre == "name"
-      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
-      @user_length = User.where('name LIKE(?)',"%#{params[:keyword]}%").size
-      @note_length = Note.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).size
-      @study_log_length = StudyLog.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).size
-      # 検索する処理
-      @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").limit(LIMIT)
-      @notes = Note.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).limit(LIMIT)
-      @study_logs = StudyLog.joins(:user).where(user_id: User.where('name LIKE(?)',"%#{params[:keyword]}%")).limit(LIMIT)
-    # ノート検索
-    elsif genre == "note"
-      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
-      @user_length = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").size
-      @study_log_length = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").size
-      @note_length = Note.where('title LIKE(?)',"%#{params[:keyword]}%").size
-      # 検索する処理
-      @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
-      @study_logs = StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").limit(LIMIT)
-      @notes = Note.where('title LIKE(?)',"%#{params[:keyword]}%").limit(LIMIT)
-
-    # タグ検索(名前は引かない)
-    elsif genre =="tag"
-      # ページネーションのボタンをdisabledにするため、取得した要素の件数を取得
-      @user_log_length = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").size
-      @study_log_length = StudyLog.tagged_with(params[:keyword]).size
-      @note_length = Note.tagged_with(params[:keyword]).size
-      # 検索する処理
-      @users = User.all.includes([:relationships,:reverse_of_relationships]).order(id: "DESC").limit(LIMIT)
-      @study_logs =  StudyLog.all.includes([:user,:study_log_favorites,:tags]).order(id: "DESC").limit(LIMIT)
-      @notes = Note.tagged_with(params[:keyword]).limit(LIMIT)
-    end
-    # タグ検索
-    respond_to do |format|
-      format.json
-    end
-  end
 end
