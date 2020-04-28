960cb72ce1249fc5b065e8e1f0f603c307700c80
funeace <funeace@gmail.com>
2020-04-28 13:14:34 +0000

    [Fix]sign_in時のエラーメッセージを修正しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,17 @@ module ApplicationHelper
         "info"
       else
         flash_type.to_s
-      end
     end
+  end
+  
+  def devise_error_messages
+    return "" if resource.errors.empty?
+    errors_html = ""
+    resource.errors.full_messages.each do |err_msg|
+      errors_html += <<-EOF
+          <div class="alert-danger pl-3">#{err_msg}</div>
+      EOF
+    end
+    errors_html.html_safe
+  end  
 end
