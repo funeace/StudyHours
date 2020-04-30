1a3f37dbd4beeef56287e523e3bd73e4de8441c3
funeace <funeace@gmail.com>
2020-04-30 01:35:02 +0000

    [Fix]Roomコントローラをリファクタリングしました

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  LIMIT = 6    
+  LIMIT = 6
 
   # チャットするユーザ一覧
   def index
@@ -20,7 +20,7 @@ class RoomsController < ApplicationController
   def create
     # ルームに入る際、必ずcreateアクションを経由させる
     @user = User.find(params[:user_id])
-    if user_signed_in? 
+    if user_signed_in?
       current_entry = Entry.where(user_id: current_user.id)
       partner_entry = Entry.where(user_id: @user.id)
 
@@ -39,27 +39,28 @@ class RoomsController < ApplicationController
         unless @room_chk == true
           @new_room = Room.create
           # binding.pry
-          entry1 = Entry.create(user_id: @user.id,room_id: @new_room.id)
-          entry2 = Entry.create(user_id: current_user.id,room_id: @new_room.id)
+          entry1 = Entry.create(user_id: @user.id, room_id: @new_room.id)
+          entry2 = Entry.create(user_id: current_user.id, room_id: @new_room.id)
           redirect_to room_path(@new_room.id)
         end
       end
     end
   end
 
-
-# APIの処理
+  # 以下APIの処理
   # インクリメンタルサーチ
   def search
     # binding.pry
-    users_base = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id: current_user.id)
+    users_base = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+                     .where.not(id: current_user.id)
+
     @users_length = users_base.size
     @users = users_base.limit(LIMIT)
     respond_to do |format|
       format.json
     end
   end
-  
+
   # インクリメンタルサーチを解除する処理
   def return
     @users = User.all.where.not(id: current_user.id).limit(LIMIT)
@@ -71,7 +72,11 @@ class RoomsController < ApplicationController
   def more
     offset = params[:offset]
     # binding.pry
-    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id: current_user.id).limit(LIMIT).offset(offset)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+                 .where.not(id: current_user.id)
+                 .limit(LIMIT)
+                 .offset(offset)
+
     respond_to do |format|
       format.json
     end
