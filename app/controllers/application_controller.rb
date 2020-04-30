310c8b2e8fbd892dbb8d7479a99e8df7d4b2a1cc
funeace <funeace@gmail.com>
2020-04-30 01:51:06 +0000

    [Fix]通知コントローラ及びapplicationコントローラをリファクタリングしました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,21 @@ class ApplicationController < ActionController::Base
   before_action :set_host
 
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
-    end
 
-    def after_sign_in_path_for(resource)
-      case resource
-      when Admin
-        admins_users_path
-      when User
-        timelines_path
-      end
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
-    def set_host
-      Rails.application.routes.default_url_options[:host] = request.host_with_port
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admins_users_path
+    when User
+      timelines_path
     end
+  end
 
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.host_with_port
+  end
 end
